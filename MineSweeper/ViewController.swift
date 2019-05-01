c9285ea18fde11c5530d1bd649b2cd3ad053b139
Kyle Chung <kchung1068@stu.d214.org>
2019-05-01 14:28:48 -0500

    Initial Commit

---
+++ MineSweeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MineSweeper
+//
+//  Created by Kyle Chung on 5/1/19.
+//  Copyright © 2019 Kyle Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
