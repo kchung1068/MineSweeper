bc68ac11908abd3b2fe28ff693e8f3e7cf2e15e0
zach calcagno <zcalcagno2480@stu.d214.org>
2019-05-07 14:29:25 -0500

    fix thing

--- MineSweeper/ViewController.swift
+++ MineSweeper/ViewController.swift
@@ -52,7 +52,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func showSafariVC(for url: String) {
         guard let url = URL(string: url) else {return}
         let safariVC = SFSafariViewController(url: url)
-        present(safariVC, animated: true)
+        present(safariVC, animated: true, completion: nil)
     }
 
 }
