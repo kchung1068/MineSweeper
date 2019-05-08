ad030859a3897f7024cf5fb5b75e558d456c005a
zach calcagno <zcalcagno2480@stu.d214.org>
2019-05-08 14:41:03 -0500

    added menu help button

--- MineSweeper/menuViewController.swift
+++ MineSweeper/menuViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import SafariServices
 
 class menuViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
-
-
+    @IBAction func menuHelpButton(_ sender: Any) {
+        showSafariMenuVC(for: "https://www.wikihow.com/Play-Minesweeper")
+    }
+    
+    func showSafariMenuVC(for url: String) {
+        
+        guard let url = URL(string: url) else
+            
+        {return}
+        
+        let safariMenuVC = SFSafariViewController(url: url)
+        
+        present(safariMenuVC, animated: true, completion: nil)
+    }
+ 
 }
+
+
+
