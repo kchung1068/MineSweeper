58da5bc22a44781db45672feb1724f449193b03a
Sam <sschuffler1332@stu.d214.org>
2019-05-13 15:09:39 -0500

    did nothing

--- MineSweeper/ViewController.swift
+++ MineSweeper/ViewController.swift
@@ -17,23 +17,17 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-       
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as!
-            CollectionViewCell
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as!
+            CollectionViewCell 
         cell.cellImageView.image = UIImage(named: "1")
         cell.backgroundColor = .blue
         cell.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap)))
         cell.tag = indexPath.row
-        if cell.alpha == 0.0 {
-            print(cell.tag)
-        }
         if cell.tag == 73 {
             cell.cellImageView.image = UIImage(named: "1")
         }
         
         return cell
-        
         
         
         
@@ -52,8 +46,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     @objc func tap(sender: UITapGestureRecognizer){
         print("erg")
+       // collectionView.alpha = 0
         
-        present(youLose, animated: true, completion: nil)
+      //  present(youLose, animated: true, completion: nil)
     }
     
     var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
