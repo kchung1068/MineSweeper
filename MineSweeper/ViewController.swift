6f822302b62a360a18be387c6bd4c1438158cf86
Sam <sschuffler1332@stu.d214.org>
2019-05-17 14:59:09 -0500

    accessed if it is a 1

--- MineSweeper/ViewController.swift
+++ MineSweeper/ViewController.swift
@@ -68,6 +68,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
                 alert.addAction(okAction)
                 present(alert,animated: true,completion: nil)
         }
+        if (arrays[currentCell.tag]) == String("1"){
+            print("Zach is cool")
+        }
         
        // present(youLose, animated: true, completion: nil)
     }
