20d0ce12bfceec749072630eed23976dcc47dc5c
Ryan Kaiser <rkaiser0176@stu.d214.org>
2019-05-17 14:26:33 -0500

    erased code that wasn't needed

--- MineSweeper/ViewController.swift
+++ MineSweeper/ViewController.swift
@@ -61,7 +61,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         print(arrays[currentCell.tag])
         let formula = UIImage(named: arrays[currentCell.tag])
         currentCell.cellImageView.image = formula
-        if (arrays[currentCell.tag]) == String("Bomb"){ print("here")
+        if (arrays[currentCell.tag]) == String("Bomb"){ 
             
                 let alert = UIAlertController(title: "You Lose", message: nil, preferredStyle: .alert)
                 let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
@@ -69,7 +69,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
                 present(alert,animated: true,completion: nil)
         }
         
-       // present(youLose, animated: true, completion: nil)
+       
     }
     
     
@@ -77,7 +77,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         // print("erg")
        // collectionView.alpha = 0
         
-      //  present(youLose, animated: true, completion: nil)
+      
     }
     
     var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
